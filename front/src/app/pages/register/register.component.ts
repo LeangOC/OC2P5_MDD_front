8016211bc326da80b8aa47ceafc5bd2f01d8c597
Leang Chouteau <chouteau.leang@gmail.com>
2026-06-16 09:26:39 +0200

    Formulaire Register

--- front/src/app/pages/register/register.component.ts
+++ front/src/app/pages/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
+  imports: [
+    FormsModule
+  ],
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+userEmail: string = 'nom@Openclassrooms';
+userName: string = '';
+onSubmitForme(): void {
+  console.log(this.userEmail);
+  console.log("Bonjour : " + this.userName);
+}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
